592d3f737d12d3a2095eb72ce3634acab30655a4
ronishoham <roni.shoham@sarine.com>
2018-01-01 11:37:11 +0200

    task 107381

--- dist/sarine.viewer.image.bundle.js
+++ dist/sarine.viewer.image.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.image - v0.5.0 -  Thursday, September 28th, 2017, 3:55:12 PM 
+sarine.viewer.image - v0.6.2 -  Monday, January 1st, 2018, 11:31:51 AM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
